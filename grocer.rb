168bef89c7a66758355692a8e8b12e770f185938
Megan Wong <mwong068@gmail.com>
2019-10-24 03:41:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ def apply_coupons(cart, coupons)
   
   coupons.each do |coupon|
     coupon_about = find_item_by_name_in_collection(coupon[:item], cart) #if coupon item exists in cart
-    coupon_about_w_coupon = find_item_by_name_in_collection(coupon[:item] + "w/coupon", cart) #if cart already contains coupon
+    coupon_about_w_coupon = find_item_by_name_in_collection(coupon[:item] + "W/COUPON", cart) #if cart already contains coupon
       if coupon_about[:count] && coupon_about_w_coupon >= coupon[:num]   #if cart contains coupon item and the coupon num is larger than the current amount && contains item w/ coupon
         coupon_about_w_coupon[:count] += coupon[:num]
         coupon_about[:count] -= coupon[:num]
